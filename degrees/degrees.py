84e9789fbf673f07993ca908d9e66253f50dd743
RJ Shoemaker <rjshoemaker55@gmail.com>
2021-01-07 21:02:46 -0500

    project working

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -52,6 +52,35 @@ def load_data(directory):
                 pass
 
 
+class Node():
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
 def main():
     if len(sys.argv) > 2:
         sys.exit("Usage: python degrees.py [directory]")
@@ -84,16 +113,78 @@ def main():
             print(f"{i + 1}: {person1} and {person2} starred in {movie}")
 
 
+# def shortest_path(source, target):
+
+#     # initialize a variable to hold the number of explored person_ids
+#     num_explored = 0
+#     # initialize a starting node with the source person_id as the state, no parent (first node) and no action (first node)
+#     start = Node(state=source, parent=None, action=None)
+#     # initializea  new frontier from the stack frontier
+#     frontier = StackFrontier()
+#     # add the starting node (state, parent, action) to the frontier
+#     frontier.add(start)
+#     # intialize a new set to hold the explored nodes
+#     explored = set()
+
+#     while(True):
+
+#          """ 1. If the frontier is empty, all people have been explored and there is no solution"""
+#         if frontier.empty():
+#             return None
+
+#         """ 2. Remove a node from the frontier """
+#         node = frontier.remove()
+
+#         # add the removed node to the explored set
+#         explored.add(node.state)
+
+#         # explore each person that has starred in movies with this person, pull out the movie_id and person_id for each
+#         for movie_id, person_id in neighbors_for_person(node.state):
+#             # if this person is not in the explored set or the frontier,
+#             if (not frontier.contains_state(person_id) and person_id not in explored):
+#                 """ 3. If the node is a goal state, return the solution """
+#                 if person_id == target:
+#                     # create a new node for this person, setting the state as the id, parent as the current node, and action as the movie_id (how they are connected)
+#                     n = Node(state=person_id, parent=node, action=movie_id)
+#                     # intialize a new actions list to store the actions that got us to the goal node
+#                     actions = []
+#                     # start reversing the actions to build a list of the actions that got us here
+#                     # while there is a parent node for the current node,
+#                     while(n.parent is not None):
+#                         # append the current node to the actions list
+#                         actions.append((n.action, n.state))
+#                         # set the parent node to the current node, repeat the process
+#                         n = n.parent
+#                     # once we have build up the list, reverse it so it is in the correct order
+#                     actions.reverse()
+#                     # return the reversed actions list
+#                     return actions
+
 def shortest_path(source, target):
-    """
-    Returns the shortest list of (movie_id, person_id) pairs
-    that connect the source to the target.
+    num_explored = 0
+    start = Node(state=source, parent=None, action=None)
+    frontier = StackFrontier()
+    frontier.add(start)
+    explored = set()
 
-    If no possible path, returns None.
-    """
+    while True:
+        if frontier.empty():
+            return None
 
-    # TODO
-    raise NotImplementedError
+        node = frontier.remove()
+        explored.add(node)
+
+        for movie_id, person_id in neighbors_for_person(node.state):
+            if (not frontier.contains_state(person_id) and person_id not in explored):
+                if person_id == target:
+                    n = Node(state=person_id, parent=node, action=movie_id)
+                    actions = []
+
+                    while (n.parent is not None):
+                        actions.append((n.action, n.state))
+                        n = n.parent
+                    actions.reverse()
+                    return actions
 
 
 def person_id_for_name(name):
